8a79b0c7a7e0e2a425c4cd53b28c14d2d7fd312d
Rodrigo Diez Villamuera <rodrigo@rodrigodiez.io>
2016-05-11 21:04:13 +0100

    First commit

---
+++ echo.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	log.Println(r.Form.Get(command))
+}
+
+func main() {
+
+	port := flag.Int("port", 8000, "tcp port to listen")
+	flag.Parse()
+
+	http.HandleFunc("/echo", echo)
+	log.Printf("Listening on :%d", *port)
+	http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
+}
